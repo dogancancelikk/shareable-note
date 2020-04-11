87b5cd92f78b9abbc6d0a909cf65634a15d8362d
Canberk Oguz <c.oguz@elsevier.com>
2020-04-11 12:38:36 +0200

    create list and listDetail components

--- App/Navigators/AppNavigator.js
+++ App/Navigators/AppNavigator.js
@@ -1,6 +1,6 @@
 import { createAppContainer, createStackNavigator } from 'react-navigation'
-
-import ExampleScreen from 'App/Containers/Example/ExampleScreen'
+import ListsScreen from 'App/Containers/Lists/ListsScreen'
+import ListDetailScreen from 'App/Containers/ListDetail/ListDetailScreen'
 
 /**
  * The root screen contains the application's navigation.
@@ -12,9 +12,8 @@ const StackNavigator = createStackNavigator(
     // Create the application routes here (the key is the route name, the value is the target screen)
     // See https://reactnavigation.org/docs/en/stack-navigator.html#routeconfigs
     //SplashScreen: SplashScreen,
-    // The main application screen is our "ExampleScreen". Feel free to replace it with your
-    // own screen and remove the example.
-    MainScreen: ExampleScreen,
+    MainScreen: ListsScreen,
+    ListDetailScreen: ListDetailScreen,
   },
   {
     // By default the application will show the splash screen
